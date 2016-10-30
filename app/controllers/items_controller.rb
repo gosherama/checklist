55e0095736403304a769fe8083ec513fa383f690
gosher <macciak@gmail.com>
2016-10-30 14:59:13 +0000

    CRUD

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,52 @@
 class ItemsController < ApplicationController
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
+    
     def index
+        @items = Item.all.order('created_at DESC')
     end
     
     def new
         @item = Item.new
     end
     
-    def create
+    def show
     end
+    
+    def create
+        @item = Item.new(items_params)
+        
+        if @item.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @item.update(items_params)
+            redirect_to item_path(@item)
+        else 
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @item.destroy
+        redirect_to root_path
+    end
+    
+    
+ private
+     
+     def items_params
+        params.require(:item).permit(:title, :description)
+     end
+     
+     def find_item
+        @item = Item.find(params[:id])
+     end
+
 end
